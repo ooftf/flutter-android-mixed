cd3ea9413d93e85eef3dc2aa40203e47382d7ff0
ooftf <li994749769@gmail.com>
2019-12-24 20:42:25 +0800

    修改环境

--- lib/ListDemo.dart
+++ lib/ListDemo.dart
@@ -1,11 +1,15 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 class ListDemoWidget extends StatelessWidget {
+  final _items  = <WordPair>[];
+  final _favorite = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, i) {
-      return Center(child: Text(i.toString()));
+    return ListView.builder(itemBuilder: (context, position) {
+
+      return new ListTile(title: new Text(_items[position].asPascalCase,style: _biggerFont));
     });
   }
 }
